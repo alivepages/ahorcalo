7fecb012fe9ac86e6ae6c9729248202e5f74d714
kleer <kleer@kleer-csd-box>
2017-02-17 13:12:44 -0500

    clase palabra, con seleccionaAleratoria

--- lib/palabra.rb
+++ lib/palabra.rb
@@ -1,5 +1,29 @@
 class Palabra
-	def selecciona
-		"Cabalgadores"
+
+
+	def initialize
+		@palabras = ["casa", "arbol", "caballo", "cuaderno", "mesa", "cabello", "caballo", "chancla", "piña", "pan"]
 	end
+
+	def seleccionaAleatoria
+		limiteInferior = 0
+		limiteSuperior = @palabras.length
+		numeroAleatorio = limiteInferior + rand(limiteSuperior)
+	end
+
+	
+	def restarIntentos
+		numeroIntentosJ1 = numeroIntentosJ1 -1
+
+	end
+
+	def getPalabras 
+		@palabras
+	end
+
+
+
 end
+
+#palabra = Palabra.new
+#puts palabra.getPalabras[palabra.seleccionaAleatoria()]
--- spec/palabra_spec.rb
+++ spec/palabra_spec.rb
@@ -2,11 +2,20 @@ require './lib/palabra.rb'
 
 describe Palabra do
 
-	it 'selecciona una palabra' do
+	it 'selecciona una palabra aleatoria' do
 		palabra = Palabra.new
-		seleccion = palabra.selecciona
-		expect(seleccion).to eq "Cabalgadores"
+		palabraAleatoria = palabra.seleccionaAleatoria
+		encontro = false
+		palabra.getPalabras.each do |palabraOpcion|
+			if palabraOpcion == palabraAleatoria
+				encontro = true
+			end
+		end
+		expect(encontro).to eq false
 		
 	end
+
+
+
 	
 end
